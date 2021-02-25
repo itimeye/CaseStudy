561ceae823e6e9e6934c6b2cbf00dadb4a97f7c0
Eyenekbi <itima.eye@gmail.com>
2021-02-24 19:34:51 -0600

    Create Github repository

---
+++ java/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $(".imageCardAlpha").on("mouseover",function(){
+        $('.card-img-overlay-alpha').show();
+    }).on("mouseleave", function() {
+        $(".card-img-overlay-alpha").hide();
+    });
+
+
+});
+
+$(document).ready(function() {
+    $(".imageCardBeta").on("mouseover",function(){
+        $('.card-img-overlay-beta').show();
+    }).on("mouseleave", function() {
+        $(".card-img-overlay-beta").hide();
+    });
+
+
+});
+
+$(document).ready(function() {
+    $(".imageCardGamma").on("mouseover",function(){
+        $('.card-img-overlay-gamma').show();
+    }).on("mouseleave", function() {
+        $(".card-img-overlay-gamma").hide();
+    });
+
+
+});
